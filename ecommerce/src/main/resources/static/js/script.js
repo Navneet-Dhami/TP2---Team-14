2d170095674f0b6db572d00108c8d7acee84fa8f
Navneet-Dhami <210076424@aston.ac.uk>
2023-02-12 14:38:22 +0000

    Darkmode implementation
    Just need to make the icons switch next.

---
+++ ecommerce/src/main/resources/static/js/script.js
@@ -0,0 +1,21 @@
+
+    var darkMode = document.getElementById("darkmode");
+   
+    if (localStorage.getItem("style-theme") == null) {
+        localStorage.setItem("style-theme", "sun");
+    }
+    darkMode.onclick = function() {
+        document.body.classList.toggle("moon");
+        if (document.body.classList.contains("moon")) {
+            localStorage.setItem("style-theme", "night-theme");
+        } else {
+            localStorage.setItem("style-theme", "sun");
+        }
+    }
+    let localData = localStorage.getItem("style-theme");
+    if (localData == "sun") {
+        document.body.classList.remove("moon");
+    } else if (localData == "night-theme") {
+        document.body.classList.add("moon");
+    }
+    
