07c825b491aa154198036010d8914aad5af31dbf
Mohamed Rilah <210049037@aston.ac.uk>
2023-02-16 15:44:48 +0000

    Updated Controller

--- ecommerce/src/main/java/team14/ecommerce/controller/MainController.java
+++ ecommerce/src/main/java/team14/ecommerce/controller/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(path = "/")
 public class MainController {
 
-    @GetMapping
+    @GetMapping("/")
     public String home(){
         return "home.html";
     }
