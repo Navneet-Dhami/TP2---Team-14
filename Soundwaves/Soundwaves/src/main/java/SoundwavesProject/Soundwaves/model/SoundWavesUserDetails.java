174f849843693835f93deb85c13486060af4a872
Mohamed Rilah <210049037@aston.ac.uk>
2023-02-23 14:24:26 +0000

    Added UserDetails Model

---
+++ Soundwaves/Soundwaves/src/main/java/SoundwavesProject/Soundwaves/model/SoundWavesUserDetails.java
@@ -0,0 +1,57 @@
+package SoundwavesProject.Soundwaves.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SoundWavesUserDetails implements UserDetails {
+
+    private User u;
+    
+    public SoundWavesUserDetails(User u) {
+        this.u = u; 
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String role = u.getRole();
+        List<SimpleGrantedAuthority> auth = new ArrayList<>(); 
+
+        auth.add(new SimpleGrantedAuthority(role));
+        return auth;
+    }
+
+    @Override
+    public String getPassword() {
+        return u.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return u.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }   
+}
