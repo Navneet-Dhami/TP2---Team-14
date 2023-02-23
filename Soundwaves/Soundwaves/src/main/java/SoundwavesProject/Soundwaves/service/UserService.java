a1830f2a07a06eed8a4fbd2a56251274fda2c181
Mohamed Rilah <210049037@aston.ac.uk>
2023-02-23 14:25:47 +0000

    Updated UserService

--- Soundwaves/Soundwaves/src/main/java/SoundwavesProject/Soundwaves/service/UserService.java
+++ Soundwaves/Soundwaves/src/main/java/SoundwavesProject/Soundwaves/service/UserService.java
@@ -1,7 +1,24 @@
 package SoundwavesProject.Soundwaves.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
-    public UserDetails findUserByEmail(String email);    
+@Service
+public class UserService implements UserDetailsService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User u = userRepository.findByUsername(username);
+
+        if (u == null) { 
+            throw new UsernameNotFoundException(username);
+        }
+        return new SoundWavesUserDetails(u);
+    }   
 }
