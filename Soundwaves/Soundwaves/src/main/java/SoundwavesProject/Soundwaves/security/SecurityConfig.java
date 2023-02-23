483cb80e00757ea615bb50936a3979bc65f08014
Mohamed Rilah <210049037@aston.ac.uk>
2023-02-23 14:30:04 +0000

    Added Spring Security

---
+++ Soundwaves/Soundwaves/src/main/java/SoundwavesProject/Soundwaves/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package SoundwavesProject.Soundwaves.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import SoundwavesProject.Soundwaves.service.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    
+    @Bean
+    public UserDetailsService userDetailsService() { 
+        return new UserService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() { 
+        return new BCryptPasswordEncoder();
+    } 
+
+    @Bean 
+    public DaoAuthenticationProvider authenticationProvider() {  
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception { 
+        http
+            .authorizeHttpRequests()
+            .requestMatchers( "/Media/**").permitAll() 
+            .requestMatchers( "/css/styles.css", "/js/script.js").permitAll()  
+            .requestMatchers("/", "/index", "/contact", "/products").permitAll()
+            .requestMatchers( "/register", "/registerUser").permitAll()    
+            .requestMatchers( "/admin/adminHome").hasAnyAuthority("ADMIN")
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().loginPage("/login")
+            .loginProcessingUrl("/loginUser")
+            .usernameParameter("loginEmail")
+            .passwordParameter("loginPassword")
+            .defaultSuccessUrl("/success")
+            .failureUrl("/")
+            .permitAll()
+            .and() 
+            .logout()
+            .logoutSuccessUrl("/")
+            .permitAll();
+        return http.build();
+    }
+}
