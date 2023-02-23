58b529109984dff75a979bbd1d3eed3ce5338b6c
Fabdi24 <210083008@aston.ac.uk>
2023-02-23 23:02:14 +0000

    Update ecom_table.sql

--- Soundwaves/Soundwaves/src/main/resources/ecom_table.sql
+++ Soundwaves/Soundwaves/src/main/resources/ecom_table.sql
@@ -79,6 +79,7 @@ CREATE TABLE users (
   email varchar(255) DEFAULT NULL,
   name varchar(255) DEFAULT NULL,
   password varchar(255) DEFAULT NULL,
+  role varchar(255) DEFAULT NULL,
   surname varchar(255) DEFAULT NULL,
   username varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
