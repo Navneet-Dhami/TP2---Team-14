573a5c4fff549ba5c723b145b1c40925835ac045
Navneet-Dhami <210076424@aston.ac.uk>
2023-03-02 08:13:47 +0000

    Revert "Revert "Merge branch 'main' of https://github.com/Navneet-Dhami/TP2---Team-14""
    This reverts commit 52a2411d7e59cb677ca9096b72168abf16675ce0.

--- Soundwaves/Soundwaves/src/main/java/SoundwavesProject/Soundwaves/security/SecurityConfig.java
+++ Soundwaves/Soundwaves/src/main/java/SoundwavesProject/Soundwaves/security/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig{
             .authorizeHttpRequests()
             .requestMatchers( "/media/**").permitAll() 
             .requestMatchers( "/css/styles.css", "/js/script.js").permitAll()  
-            .requestMatchers("/", "/index", "/contact", "/products", "/products/category/{id}","/aboutus").permitAll()
+            .requestMatchers("/", "/index", "/contact", "/products", "/products/category/{id}","/aboutus", "/search", "search_results").permitAll()
             .requestMatchers( "/register", "/registerUser").permitAll()    
             .requestMatchers( "/admin/adminHome").hasAnyAuthority("ADMIN")
             .anyRequest().authenticated()
