330f0f2afbf96e428eb4d810479d4d22f7581f24
lonk1883 <210122307@aston.ac.uk>
2023-03-02 12:09:08 +0000

    uncommented OrderDTO until further notice

--- Soundwaves/Soundwaves/src/main/java/SoundwavesProject/Soundwaves/dto/OrderDTO.java
+++ Soundwaves/Soundwaves/src/main/java/SoundwavesProject/Soundwaves/dto/OrderDTO.java
@@ -1,19 +1,19 @@
-package SoundwavesProject.Soundwaves.dto;
-import java.sql.Date;
-import lombok.Data;
+// package SoundwavesProject.Soundwaves.dto;
+// import java.sql.Date;
+// import lombok.Data;
 
 
-@Data
-public class OrderDTO {
-    private Long id;
-    private Long productId;
-    private int userId;
+// @Data
+// public class OrderDTO {
+//     private Long id;
+//     private Long productId;
+//     private int userId;
 
-    private Date createdDate;
-    private double totalPrice;
-    private String img;
-    private int quantity;
-    private int tracking;
+//     private Date createdDate;
+//     private double totalPrice;
+//     private String img;
+//     private int quantity;
+//     private int tracking;
 
     
-}
+// }
