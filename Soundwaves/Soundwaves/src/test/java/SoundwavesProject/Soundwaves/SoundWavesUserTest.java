8a1cc1d41260a1c01fa9a57607022963eaf0635d
Mohamed Rilah <210049037@aston.ac.uk>
2023-03-16 14:53:41 +0000

    Update SoundWavesUserTest.java
    Added more tests for users

--- Soundwaves/Soundwaves/src/test/java/SoundwavesProject/Soundwaves/SoundWavesUserTest.java
+++ Soundwaves/Soundwaves/src/test/java/SoundwavesProject/Soundwaves/SoundWavesUserTest.java
@@ -10,12 +10,32 @@ import SoundwavesProject.Soundwaves.model.User;
 @SpringBootTest
 public class SoundWavesUserTest {
 
+
     @Test
-	void testUser() { 
-		User u = new User();
-		u.setFirstname("User");
-		u.setSurname("Soundwaves");
-		assertEquals("User", u.getFirstname());
-		assertEquals("Soundwaves", u.getSurname());
+	void testUserDetails() { 
+		User user = new User();
+		user.setFirstname("User");
+		user.setSurname("Soundwaves");
+        user.setEmail("user@soundwaves.com");
+        user.setPassword("123456");
+        user.setUsername("soundwavesUser");
+
+		assertEquals("User", user.getFirstname());
+		assertEquals("Soundwaves", user.getSurname());
+        assertEquals("user@soundwaves.com", user.getEmail());
+        assertEquals("123456", user.getPassword());
+        assertEquals("soundwavesUser", user.getUsername());
 	}
+
+    @Test
+    void testUserRole() { 
+        User user = new User();
+        user.setRole("USER");
+
+        User admin = new User();
+        admin.setRole("ADMIN");
+
+        assertEquals("USER", user.getRole());
+        assertEquals("ADMIN", admin.getRole());
+    }
 }
